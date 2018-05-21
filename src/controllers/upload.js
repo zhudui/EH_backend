2709397f982557663d81e5255a888472b9abe9c0
zhudui <1209300584@qq.com>
2018-05-21 18:16:43 +0800

    测试上传文件接口

--- src/controllers/upload.js
+++ src/controllers/upload.js
@@ -41,14 +41,17 @@ export default {
     let tempfilepath = files.file.path
     // 获取文件类型
     let type = files.file.type
+    console.log('文件类型', type);
 
     // 获取文件名，并根据文件名获取扩展名
     let filename = files.file.name
+    console.log('文件名', filename);
     let extname = filename.lastIndexOf('.') >= 0 ? filename.slice(filename.lastIndexOf('.') - filename.length) : ''
     // 文件名没有扩展名时候，则从文件类型中取扩展名
     if (extname === '' && type.indexOf('/') >= 0) {
       extname = '.' + type.split('/')[1]
     }
+    console.log('扩展名', extname);
     // 将文件名重新赋值为一个随机数（避免文件重名）
     filename = shortId.generate() + extname
 
